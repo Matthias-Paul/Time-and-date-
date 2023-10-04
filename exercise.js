82f2d12c3a04d4f8deb6802b55cfa27c865e2e6f
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2023-10-04 12:20:17 +0100

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,34 @@
+var button = document.getElementById("button")
+var output = document.getElementById("output")
+
+button.addEventListener("click", function(){
+  
+  output.innerHTML=Date()
+  console.log(output)
+  
+  
+})
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
